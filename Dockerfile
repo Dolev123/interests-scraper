c75e51758e7f64a540e86932aac8dfabeef8281b
Dolev123 <dolev.niv.2@gmail.com>
2025-06-04 21:09:27 +0300

    Add Dockerfile and run.sh for entry

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM alpine:latest
+
+RUN apk add vim git python3
+
+RUN adduser -u 1001 -D user
+WORKDIR /home/user
+USER 1001
+
+RUN git clone https://github.com/Dolev123/interests-scraper.git
+WORKDIR //home/user/interests-scraper
+RUN python -m venv . && . bin/activate && pip install -r requirements.txt
+
+EXPOSE 8080/tcp
+
+CMD ["/bin/sh", "./run.sh"]
---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+export WORK_DIR=
+export SRV_DIR=/tmp/srv
+
+function check_srv_dir() {
+    if [ ! -d "$SRV_DIR" ]; then
+	echo "[+] Creating server directory '$SRV_DIR'"
+	mkdir $SRV_DIR
+    fi
+}
+
+function start_http_server() {
+    check_srv_dir
+    cd $SRV_DIR
+    echo "[+] Starting Server"
+    ip a
+    python -m http.server 8080 &
+}
+
+function update_report() {
+    while :
+    do
+	echo "[+] Updateiong Report"
+	check_srv_dir
+	cd $WORK_DIR
+	. bin/activate
+	python main.py
+	[[ -e report.html ]] && cp report.html "$SRV_DIR/$(date +%Y-%m-%d).html" || echo "[!] Could not generate report."
+	sleep 2h
+    done
+}
+
+start_http_server
+update_report
